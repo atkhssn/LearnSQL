8f80ce98084cecbf5346096184713d1e8a08ec0b
atkhssn <atik.hassan@outlook.com>
2023-11-24 17:05:58 +0600

    DDL commands - create table

--- LearnDatabase.sql
+++ LearnDatabase.sql
@@ -6,3 +6,12 @@
 	-- 2. DROP DATABASE:
 		-- DROP DATABASE DatabaseName;
 			DROP DATABASE TestDatabase;
+
+	-- 3. CREATE TABLE:
+		-- CREATE TABLE TableName;
+		USE TestDatabase; -- Use keyeword is used for making a query operation into the specific database
+		CREATE TABLE PersonTable(
+			PersonTableId INT PRIMARY KEY,
+			FristName VARCHAR(30) NOT NULL,
+			LastName VARCHAR(30) NOT NULL
+		);
